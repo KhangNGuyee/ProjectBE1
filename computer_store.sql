46c1d97e98955eecc7e68d383d22094bf526c062
KhangNGuyee <135322305+KhangNGuyee@users.noreply.github.com>
2024-11-14 20:54:31 +0700

    db

--- computer_store.sql
+++ computer_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2024 at 02:44 PM
+-- Generation Time: Nov 14, 2024 at 02:54 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
